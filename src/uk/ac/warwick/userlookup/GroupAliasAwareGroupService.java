590c3451e6c04f35539f17051f0b2e08a19c05e2
Mat Mannion <m.mannion@warwick.ac.uk>
2012-06-01 15:10:39 +0000

    SSO-1223

--- src/uk/ac/warwick/userlookup/GroupAliasAwareGroupService.java
+++ src/uk/ac/warwick/userlookup/GroupAliasAwareGroupService.java
@@ -9,7 +9,7 @@ import uk.ac.warwick.userlookup.webgroups.GroupServiceAdapter;
 import uk.ac.warwick.userlookup.webgroups.GroupServiceException;
 
 /**
- * GroupService that knows about the magic ANYONE, MEMBER, STAFF, STAFFNOPGR, PGRESEARCH, ALUMNI and STUDENT groups.
+ * GroupService that knows about the magic ANYONE, MEMBER, STAFF, STAFFNOPGR, PGRESEARCH, STUDENTNOPGT, PGTAUGHT, ALUMNI and STUDENT groups.
  *
  * @author xusqac
  */
@@ -27,12 +27,18 @@ public final class GroupAliasAwareGroupService extends GroupServiceAdapter imple
     
     public static final String STUDENT = "STUDENT";
     
+    public static final String STUDENTNOPGT = "STUDENTNOPGT";
+    
     public static final String PGRESEARCH = "PGRESEARCH";
+    
+    public static final String PGTAUGHT = "PGTAUGHT";
 
     private static final String WARWICKITSCLASS_KEY = "warwickitsclass";
 	private static final String PGRSTUDENT_CLASS = "PG(R)";
+	private static final String PGTSTUDENT_CLASS = "PG(T)";
 	private static final String WARWICKCATEGORY_KEY = "warwickcategory";
 	private static final String RESEARCH_CATEGORY = "R";
+	private static final String TAUGHT_CATEGORY = "T";
 	
 	private static final Logger LOGGER = Logger.getLogger(GroupAliasAwareGroupService.class);
     
@@ -66,6 +72,12 @@ public final class GroupAliasAwareGroupService extends GroupServiceAdapter imple
         if (STUDENT.equalsIgnoreCase(groupName) && user.isStudent()) {
             return true;
         }
+        if (STUDENTNOPGT.equalsIgnoreCase(groupName) && userIsStudentNotPGT(user)) {
+        	return true;
+        }
+        if (PGTAUGHT.equalsIgnoreCase(groupName) && userIsPGT(user)) {
+            return true;
+        }
         if (ALUMNI.equalsIgnoreCase(groupName) && user.isAlumni()) {
             return true;
         }
@@ -92,6 +104,19 @@ public final class GroupAliasAwareGroupService extends GroupServiceAdapter imple
 	private boolean hasPGR(User user) {
 		return PGRSTUDENT_CLASS.equals(user.getExtraProperty(WARWICKITSCLASS_KEY)) 
 			|| RESEARCH_CATEGORY.equals(user.getExtraProperty(WARWICKCATEGORY_KEY));
+	}
+
+    private boolean userIsStudentNotPGT(User user) {
+		return (user.isStudent() && !hasPGT(user));
+	}
+    
+    private boolean userIsPGT(User user) {
+        return (user.isStudent() && hasPGT(user));
+    }
+
+	private boolean hasPGT(User user) {
+		return PGTSTUDENT_CLASS.equals(user.getExtraProperty(WARWICKITSCLASS_KEY)) 
+			|| TAUGHT_CATEGORY.equals(user.getExtraProperty(WARWICKCATEGORY_KEY));
 	}
 
 	public final List<String> getUserCodesInGroup(final String group) throws GroupServiceException {
