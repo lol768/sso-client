5c2fd1eff73d227347d2521038b44b997a625655
Mat Mannion <m.mannion@warwick.ac.uk>
2012-11-12 10:49:34 +0000

    Large refactor of source locations, moved everything to maven

---
+++ README.markdown
@@ -0,0 +1,43 @@
+SSO Client
+=============
+
+A Java library for reading from SSO in other apps.
+
+Pushing a release onto Nexus
+-------------
+
+Examples are for command line in UNIX.
+
+First, set the version number:
+
+    $ mvn versions:set -DnewVersion=`date +%Y%m%d`
+    $ mvn versions:commit
+    
+This should set <version>the-current-date</version> in all the pom.xml files.
+
+You _can_ then just run the deploy goal from the command line, but it's better to use the deploy *profile* as
+this may trigger other changes:
+
+    $ mvn -Pdeploy
+    
+Multiple versions on the same day
+-------------
+
+If you get a message like this:
+
+    [ERROR] Failed to execute goal [...] Failed to deploy artifacts: Could not transfer artifact [...] from/to nexus [...] Return code is: 400, ReasonPhrase:Bad Request
+    
+It probably means that the artifact already exists on Nexus, and Nexus doesn't support overwriting of artifacts
+with the same version. The easiest way to fix this is to add an hour/minute component to your versioning:
+
+    $ mvn versions:set -DnewVersion=`date +%Y%m%d-%H%M`
+    $ mvn versions:commit
+
+You should then be able to deploy.    
+    
+Generating distributables without pushing to Nexus
+-------------
+
+The dist profile will put JARs in a dist directory off the project root:
+
+    $ mvn -Pdist
---
+++ src/test/java/uk/ac/warwick/userlookup/UserLookupTest.java
@@ -0,0 +1,102 @@
+package uk.ac.warwick.userlookup;
+
+import static java.util.Collections.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.collection.IsMapContaining.*;
+import static org.junit.Assert.*;
+import static uk.ac.warwick.sso.client.TestMethods.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.Test;
+
+@SuppressWarnings("unchecked")
+public class UserLookupTest {
+	Mockery mockery = new Mockery();
+	
+	private TestSentryServer sentry;
+	private UserLookup ul;
+	
+	@Before public void before() {
+		 sentry = new TestSentryServer();
+		 ul = new UserLookup();
+		 ul.setSsosUrl(sentry.getPath());
+	}
+	
+	/**
+	 * Asserts that when some lookup fails due to a backend problem, UserLookup
+	 * will catch the exception and return an UnverifiedUser.
+	 */
+	@Test public void returnsUnverifiedUserOnFailure() throws Exception {
+		sentry.willReturnErrors();
+		sentry.run(new Runnable(){ public void run() {
+			assertThat("user by id", ul.getUserByUserId("cusaab"), is(unverified()) );
+			assertThat("user by token", ul.getUserByToken("abcdefg"), is(unverified()) );
+			assertThat("user by number", ul.getUserByWarwickUniId("01234567"), is(unverified()));
+			
+			//fetching multiple users
+			Map<String, User> usersMap = ul.getUsersByUserIds(Arrays.asList("aaa","bbb"));
+			assertThat(usersMap, hasEntry(is("aaa"), is(unverified()) ));
+			assertThat(usersMap, hasEntry(is("bbb"), is(unverified()) ));
+			
+			List<User> filtered = ul.findUsersWithFilter(mapFrom("sn","Aigabe"));
+			assertTrue(filtered.isEmpty());
+		}});
+	}
+	
+	/**
+	 * Searching for a user by Warwick ID should then do a sentry lookup in
+	 * order to get the more detailed User object. This test checks that this
+	 * happens and that we don't end up using a cached version.
+	 */
+	@Test public void getUserByWarwickUniId() throws Exception {
+		Map<String, String> searchResult = new HashMap<String, String>();
+		searchResult.put("sn","Howes");
+		searchResult.put("givenName", "Nick");
+		searchResult.put("warwickuniid", "1234567");
+		searchResult.put("cn", "cusebr");
+		searchResult.put("mail", "n.howes@warwick.ac.uk");
+		
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("returnType","4");
+		result.put("lastname","Howes");
+		result.put("firstname", "Nick");
+		result.put("id", "1234567");
+		result.put("user", "cusebr");
+		result.put("warwickitsclass", "Staff");
+		
+		sentry.setSearchResults(singletonList(searchResult));
+		sentry.setResults(singletonList(result));
+		sentry.run(new Runnable(){ public void run() {
+			User nick = ul.getUserByWarwickUniId("1234567");
+			assertEquals("Staff", nick.getExtraProperty("warwickitsclass"));
+		}});
+	}
+	
+	@Test public void returnsAnonymousUserWhenNotFound() throws Exception {
+		sentry.willReturnUsers(new AnonymousUser());
+		sentry.run(new Runnable() { public void run() {
+			User anon = ul.getUserByUserId("invisibleman");
+			assertThat(anon, is(anonymous()));
+		}});
+	}
+	
+	@Test public void returnsAnonymousUsersWhenSomeNotFound() throws Exception {
+		sentry.willReturnUsers(user("alan"), user("boris"));
+		sentry.run(new Runnable() {
+			public void run() {
+				Map<String, User> result = ul.getUsersByUserIds(Arrays.asList("alan","gobald","whoareyou","boris"));
+				assertThat(result, hasEntry(is("alan"), is(found()) ));
+				assertThat(result, hasEntry(is("boris"), is(found()) ));
+				assertThat(result, hasEntry(is("gobald"), is(anonymous()) ));
+				assertThat(result, hasEntry(is("whoareyou"), is(anonymous()) ));
+			}
+		});
+	}
+}
