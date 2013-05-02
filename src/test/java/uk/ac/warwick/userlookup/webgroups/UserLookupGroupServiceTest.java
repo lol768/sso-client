d21f89e8a39ea2f70b9ed403f7032e073d20eff9
Matthew Jones <Matthew.Jones@warwick.ac.uk>
2013-05-02 15:13:23 +0100

    SSO-1371

--- src/test/java/uk/ac/warwick/userlookup/webgroups/UserLookupGroupServiceTest.java
+++ src/test/java/uk/ac/warwick/userlookup/webgroups/UserLookupGroupServiceTest.java
@@ -1,12 +1,16 @@
 package uk.ac.warwick.userlookup.webgroups;
 
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 
-import uk.ac.warwick.userlookup.Group;
 import uk.ac.warwick.userlookup.GroupService;
 import uk.ac.warwick.userlookup.UserLookup;
-import junit.framework.TestCase;
+import uk.ac.warwick.userlookup.cache.Cache;
 
 public class UserLookupGroupServiceTest extends TestCase {
 	
@@ -23,10 +27,22 @@ public class UserLookupGroupServiceTest extends TestCase {
 		UserLookup userLookup = new UserLookup();
 		userLookup.setGroupServiceBackend(backend);
 		try {
-			Group groupByName = userLookup.getGroupService().getGroupByName(nonExistentGroupName);
+			userLookup.getGroupService().getGroupByName(nonExistentGroupName);
 			fail("Should have thrown exception");
 		} catch (GroupNotFoundException e) {
 			
 		}
+	}
+	
+	public void testCachesCorrectReturned() {
+		UserLookup userLookup = new UserLookup();
+		userLookup.setGroupServiceLocation("http://webgroups.warwick.ac.uk");
+		Map<String, Set<Cache<?, ?>>> caches = userLookup.getGroupService().getCaches();
+		
+		assertTrue("Caches created and returned", caches.size() > 0);
+		assertTrue("Has GROUP_CACHE_NAME", caches.containsKey(UserLookup.GROUP_CACHE_NAME));
+		assertTrue("Has IN_GROUP_CACHE_NAME", caches.containsKey(UserLookup.IN_GROUP_CACHE_NAME));
+		assertTrue("Has GROUP_MEMBER_CACHE_NAME", caches.containsKey(UserLookup.GROUP_MEMBER_CACHE_NAME));
+		assertTrue("Has USER_GROUPS_CACHE_NAME", caches.containsKey(UserLookup.USER_GROUPS_CACHE_NAME));
 	}
 }
