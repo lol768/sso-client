216df2bcf9c40157b622e28ccf5433868c03ea87
Nick Howes <n.howes@warwick.ac.uk>
2016-01-04 13:41:52 +0000

    SSO-1675 Fix behaviour of missing and unverified users.

--- modules/play/library/app/warwick/sso/UserLookupService.scala
+++ modules/play/library/app/warwick/sso/UserLookupService.scala
@@ -3,9 +3,8 @@ package warwick.sso
 import javax.inject.Inject
 
 import com.google.inject.ImplementedBy
-import uk.ac.warwick.userlookup.UserLookupInterface
+import uk.ac.warwick.userlookup.{UnverifiedUser, UserLookupInterface}
 import uk.ac.warwick.userlookup
-import uk.ac.warwick.util.core.StringUtils
 import scala.collection.JavaConverters._
 import scala.util.Try
 
@@ -33,27 +32,31 @@ trait UserLookupService {
 class UserLookupServiceImpl @Inject() (userLookupInterface: UserLookupInterface) extends UserLookupService {
   type OldUser = userlookup.User
 
+  private def existsWithUsercode(user: OldUser) = user.isFoundUser && hasUsercode(user)
+
   override def getUsers(codes: Seq[Usercode]): Try[Map[Usercode, User]] = Try {
     userLookupInterface.getUsersByUserIds(codes.map(_.string).asJava)
       .asScala
       .toMap
       .flatMap {
-      case (id, user: OldUser) if hasUsercode(user) => Some(Usercode(id), User(user))
-      case _ => None
-    }
+        case (id, user) if existsWithUsercode(user) => Some(Usercode(id), User(user))
+        case (id, user) if !user.isVerified => throw user.asInstanceOf[UnverifiedUser].getVerificationException
+        case _ => None
+      }
   }
 
   override def searchUsers(filters: Map[String, String], includeDisabled: Boolean = false) = Try {
     userLookupInterface.findUsersWithFilter(filters.asJava, includeDisabled)
       .asScala
-      .filter(hasUsercode)
+      .filter(existsWithUsercode)
       .map(User.apply)
   }
 
   override def getUsers(ids: Seq[UniversityID], includeDisabled: Boolean = false) = Try {
     // TODO Websignon supports batch lookup by university ID - implement SSO-1472 to avoid making N requests
     ids.flatMap { id =>
-      Option(userLookupInterface.getUserByWarwickUniId(id.string, includeDisabled)).filter(hasUsercode)
+      Option(userLookupInterface.getUserByWarwickUniId(id.string, includeDisabled))
+        .filter(existsWithUsercode)
     }
     .map(User.apply)
     .flatMap { user =>
@@ -64,7 +67,7 @@ class UserLookupServiceImpl @Inject() (userLookupInterface: UserLookupInterface)
 
   override def basicAuth(usercode: String, password: String) = Try {
     Option(userLookupInterface.getUserByIdAndPassNonLoggingIn(usercode, password))
-      .filter(hasUsercode)
+      .filter(existsWithUsercode)
       .map(User.apply)
   }
 
--- modules/play/library/test/warwick/sso/UserLookupServiceImplSpec.scala
+++ modules/play/library/test/warwick/sso/UserLookupServiceImplSpec.scala
@@ -1,8 +1,10 @@
 package warwick.sso
 
+import java.io.IOException
+
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import uk.ac.warwick.userlookup.{AnonymousUser, UserLookupInterface}
+import uk.ac.warwick.userlookup.{UnverifiedUser, AnonymousUser, UserLookupInterface}
 import java.util.Arrays
 import org.mockito.Matchers._
 import org.mockito.Mockito._
@@ -12,17 +14,25 @@ import scala.util.{Failure, Success}
 
 class UserLookupServiceImplSpec extends PlaySpec with MockitoSugar {
 
+  type CoreUser = uk.ac.warwick.userlookup.User
+
   class Context {
     val userlookup = mock[UserLookupInterface](withSettings().defaultAnswer(RETURNS_SMART_NULLS))
     val service = new UserLookupServiceImpl(userlookup)
     val usercodes = List(Usercode("heron"), Usercode("cuslaj"))
     val universityIds = List(UniversityID("heron"), UniversityID("1170836"))
-    val heronUser = new uk.ac.warwick.userlookup.User("heron")
+    val heronUser = new CoreUser("heron")
+    heronUser.setFoundUser(true)
     heronUser.setWarwickId("heron")
-    val ritchieUser = new uk.ac.warwick.userlookup.User("cuslaj")
+    val ritchieUser = new CoreUser("cuslaj")
+    ritchieUser.setFoundUser(true)
     ritchieUser.setWarwickId("1170836")
 
     val anon = new AnonymousUser
+    anon.setUserId("nonexist") // anon user can have an ID set
+
+    val error = new IOException("The error that caused an unverified user to be returned")
+    val unverified = new UnverifiedUser(error)
   }
   
   "UserLookupService" should {
@@ -39,6 +49,14 @@ class UserLookupServiceImplSpec extends PlaySpec with MockitoSugar {
         Map("heron" -> heronUser, "cuslaj" -> ritchieUser, "bob" -> anon).asJava
       }
       service.getUsers(usercodes).get must be(Map(Usercode("heron") -> User(heronUser), Usercode("cuslaj") -> User(ritchieUser)))
+    }
+
+    "getUsers I/O error" in new Context {
+      when (userlookup.getUsersByUserIds(Arrays.asList("ichabod"))) thenReturn {
+        Map[String, CoreUser]("ichabod" -> unverified).asJava
+      }
+
+      service.getUsers(List(Usercode("ichabod"))) must be ('failure)
     }
 
     "getUsers by university ID" in new Context {
