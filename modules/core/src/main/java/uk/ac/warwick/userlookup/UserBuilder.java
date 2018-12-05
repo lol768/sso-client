cf675c75f55c4c97704bcfb072a00f153edbd351
Adam Williams <A.Williams.9@warwick.ac.uk>
2018-12-05 11:22:25 +0000

    SSO-2275 populateUserFromSAML, populateUserFromSentry

--- modules/core/src/main/java/uk/ac/warwick/userlookup/UserBuilder.java
+++ modules/core/src/main/java/uk/ac/warwick/userlookup/UserBuilder.java
@@ -2,23 +2,43 @@ package uk.ac.warwick.userlookup;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.ac.warwick.sso.client.SAMLUserAdapter;
 
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 public final class UserBuilder {
     public static final Logger LOGGER = LoggerFactory.getLogger(UserBuilder.class);
 
+    /**
+     * @deprecated Use populateUserFromSentry
+     * @see UserBuilder#populateUserFromSentry
+     */
+    @Deprecated
     public User populateUser(final Map<String, String> results) {
+        return populateUserFromSentry(results);
+    }
+
+    public User populateUserFromSentry(final Map<String, String> results) {
+        SentryUserAdapter sentryUserAdapter = new SentryUserAdapter(results);
+        return this.populateUsingAdapter(results, sentryUserAdapter);
+    }
+
+    public User populateUserFromSAML(final Map<String, String> results) {
+        Properties props = new Properties();
+        props.putAll(results);
+        SAMLUserAdapter samlUserAdapter = new SAMLUserAdapter(props);
+        return this.populateUsingAdapter(results, samlUserAdapter);
+    }
+
+    private User populateUsingAdapter(final Map<String, String> results, final AbstractUserAttributesAdapter adapter) {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("Creating user from SSO results");
             for (Entry<String, String> entry : results.entrySet()) {
                 LOGGER.debug(entry.getKey() + "=" + entry.getValue());
             }
         }
-
-        SentryUserAdapter sentryUserAdapter = new SentryUserAdapter(results);
-
-        return AdapterUserBuilder.buildUser(sentryUserAdapter);
+        return AdapterUserBuilder.buildUser(adapter);
     }
 }
