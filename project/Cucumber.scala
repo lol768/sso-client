48bc57ffd7dc4e9020f80fba63452c8bc1821b9f
Adam Williams <A.Williams.9@warwick.ac.uk>
2018-12-20 11:05:07 +0000

    Helps if we actually use the main class

--- project/Cucumber.scala
+++ project/Cucumber.scala
@@ -7,7 +7,7 @@ object Cucumber {
     val args = Seq("-I", "modules/servlet/cucumber/lib", "modules/servlet/cucumber/bin/cucumber", "--dry-run", "--format", "html", "--out", "target/feature-spec.html", "modules/servlet/src/test/features/")
     val fo = ForkOptions().withRunJVMOptions(Vector[String]("-cp", classPath.map(_.getAbsolutePath).mkString(":")))
     println( classPath.map(_.getAbsolutePath).mkString(":"))
-    val process = sbt.Fork.java.fork(fo, args)
+    val process = sbt.Fork.java.fork(fo, mainClass +: args)
   }
 
 
