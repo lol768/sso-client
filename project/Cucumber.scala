a75fdd2ae57e89a5590a5ca433cb6cf881ed1823
Adam Williams <A.Williams.9@warwick.ac.uk>
2018-12-20 10:56:45 +0000

    SSO-1929 Try and make Cucumber work

--- project/Cucumber.scala
+++ project/Cucumber.scala
@@ -3,9 +3,11 @@ import scala.sys.process._
 
 object Cucumber {
   def run(classPath: Seq[File]) = {
-    val value = "java -cp \"" + classPath.map(_.getAbsolutePath).mkString(":") + "\" org.jruby.Main -h"
-    println(value)
-    Process (value) !
+    val mainClass = "org.jruby.Main"
+    val args = Seq("-I", "modules/servlet/cucumber/lib", "modules/servlet/cucumber/bin/cucumber", "--dry-run", "--format", "html", "--out", "target/feature-spec.html", "modules/servlet/src/test/features/")
+    val fo = ForkOptions().withRunJVMOptions(Vector[String]("-cp", classPath.map(_.getAbsolutePath).mkString(":")))
+    println( classPath.map(_.getAbsolutePath).mkString(":"))
+    val process = sbt.Fork.java.fork(fo, args)
   }
 
 
